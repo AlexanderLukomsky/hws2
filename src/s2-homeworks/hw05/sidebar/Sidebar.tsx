03ae2df073955cfe3f325413419860af9c31fe1a
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-12-18 08:37:46 +0300

    feat: t5  hamburger + nav

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,58 +1,69 @@
-import React, {FC} from 'react'
-import {NavLink} from 'react-router-dom'
-import s from './Sidebar.module.css'
-import {PATH} from '../Pages'
-import closeIcon from './closeOutline.svg'
+import React, { FC, useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import s from "./Sidebar.module.css";
+import { PATH } from "../Pages";
+import closeIcon from "./closeOutline.svg";
 
 type PropsType = {
-    open: boolean
-    handleClose: () => void
-}
+  open: boolean;
+  handleClose: () => void;
+};
 
-export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
-    const sidebarClass = s.sidebar
-        + (open ? ' ' + s.open : '')
-    return (
-        <>
-            {/*затемнение справа от открытого меню*/}
-            {open && <div className={s.background} onClick={handleClose}/>}
+export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
 
-            <aside className={sidebarClass}>
-                <button className={s.close} onClick={handleClose}>
-                    <img
-                        src={closeIcon}
-                        alt="close sidebar"
-                        id={'hw5-menu-close'}
-                    />
-                </button>
+  const [isOpenMenu, setIsOpenMenu] = useState(true);
+  let sidebarClass = s.sidebar;
+  if (open) {
+    sidebarClass = sidebarClass + ` ${s.openNav}`;
+  } else if (!isOpenMenu) {
+    sidebarClass = sidebarClass + ` ${s.closeNav}`;
+  }
 
-                <nav id={'hw5-menu'} className={s.nav}>
-                    <NavLink
-                        id={'hw5-pre-junior-link'}
-                        to={PATH.PRE_JUNIOR}
-                        onClick={handleClose}
-                        // className={...} // делает студент
-                    >
-                        Pre-junior
-                    </NavLink>
-                    <NavLink
-                        id={'hw5-junior-link'}
-                        to={PATH.JUNIOR}
-                        onClick={handleClose}
-                        // className={...} // делает студент
-                    >
-                        Junior
-                    </NavLink>
-                    <NavLink
-                        id={'hw5-junior-plus-link'}
-                        to={PATH.JUNIOR_PLUS}
-                        onClick={handleClose}
-                        // className={...} // делает студент
-                    >
-                        Junior Plus
-                    </NavLink>
-                </nav>
-            </aside>
-        </>
-    )
-}
+  useEffect(() => {
+    if (open) {
+      setIsOpenMenu(false);
+    }
+  }, [open]);
+
+  return (
+    <>
+      {/*затемнение справа от открытого меню*/}
+      {open && <div className={s.background} onClick={handleClose} />}
+
+      <aside className={sidebarClass}>
+        <button className={s.close} onClick={handleClose}>
+          <img src={closeIcon} alt="close sidebar" id={"hw5-menu-close"} />
+        </button>
+
+        <nav id={"hw5-menu"} className={s.nav}>
+          <NavLink
+            id={"hw5-pre-junior-link"}
+            to={PATH.PRE_JUNIOR}
+            onClick={handleClose}
+            className={currentPath === PATH.PRE_JUNIOR ? s.active : ""} // делает студент
+          >
+            Pre-junior
+          </NavLink>
+          <NavLink
+            id={"hw5-junior-link"}
+            to={PATH.JUNIOR}
+            onClick={handleClose}
+            className={currentPath === PATH.JUNIOR ? s.active : ""} // делает студент
+          >
+            Junior
+          </NavLink>
+          <NavLink
+            id={"hw5-junior-plus-link"}
+            to={PATH.JUNIOR_PLUS}
+            onClick={handleClose}
+            className={currentPath === PATH.JUNIOR_PLUS ? s.active : ""} // делает студент
+          >
+            Junior Plus
+          </NavLink>
+        </nav>
+      </aside>
+    </>
+  );
+};
